8c38d4a58a03ecdb83634dfe541a1a7260e8565b
Avella <kinggodavella@gmail.com>
2022-09-02 14:10:51 +0900

    1.5.2
    자리배치페이지 UI 최적화

--- src/scripts/randSeats.js
+++ src/scripts/randSeats.js
@@ -11,7 +11,7 @@ const classNames = {
     savib: saveimgButton.className
 };
 
-const goButtonRestart ='<svg class="w-1/3 h-1/3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z" clip-rule="evenodd"></path></svg>';
+const goButtonRestart ='<svg class="w-1/2 h-1/2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z" clip-rule="evenodd"></path></svg>';
 
 let restart = false;
 
@@ -192,17 +192,17 @@ function updateTable(students, columns, arrmentTable)   {
     for (let i = 0; i < parseInt(students / columns); i++)    {
         table += '<tr align="center">';
         for (let j = 0; j < columns; j++)   {
-            table += '<td class="w-16 h-16 border bg-white bg-opacity-25"><svg class="w-1/2 h-1/2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg></td>';
+            table += '<td class="w-20 h-20 border bg-white bg-opacity-25"><svg class="w-1/2 h-1/2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg></td>';
         }
         table += '</tr>';
     }
     if (students % columns != 0)    {
         table += '<tr align="center">';
         for (let i = 0; i < students % columns; i++)    {
-            table += '<td class="w-16 h-16 border bg-white bg-opacity-25"><svg class="w-1/2 h-1/2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg></td>';
+            table += '<td class="w-20 h-20 border bg-white bg-opacity-25"><svg class="w-1/2 h-1/2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg></td>';
         }
     }
-    table += '</table><br/>';
+    table += '</table>';
     arrmentTable.innerHTML = table;
 }
 
@@ -212,11 +212,11 @@ function updateTable(students, columns, arrmentTable)   {
 function resultTable(students, columns, randArr)    {
     let count = 0;
     let table = '';
-    table += '<table class="border-separate border-spacing-4 font-bold text-lg">';
+    table += '<table class="border-separate border-spacing-4 font-bold text-2xl">';
     for (let i = 0; i < parseInt(students / columns); i++)    {
         table += '<tr align="center">';
         for (let j = 0; j < columns; j++)   {
-            table += `<td class="w-16 h-16 border bg-white bg-opacity-25"> ${randArr[count]}</td>`;
+            table += `<td class="w-20 h-20 border bg-white bg-opacity-25">${randArr[count]}</td>`;
             count++;
         }
         table += '</tr>';
@@ -224,11 +224,11 @@ function resultTable(students, columns, randArr)    {
     if (students % columns != 0)    {
         table += '<tr align="center">';
         for (let i = 0; i < students % columns; i++)    {
-            table += `<td class="w-16 h-16 border bg-white bg-opacity-25"> ${randArr[count]}</td>`;
+            table += `<td class="w-20 h-20 border bg-white bg-opacity-25">${randArr[count]}</td>`;
             count++;
         }
     }
-    table += '</table><br/>';
+    table += '</table>';
     return table;
 }
 
